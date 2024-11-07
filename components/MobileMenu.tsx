459a08740f11c0d04d8dcc00626a4700648da46b
Dev Prakash Sharma <idevprakaash@gmail.com>
2024-11-08 01:12:25 +0530

    fixed menu items positioning

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -49,6 +49,7 @@ export default function MobileMenu() {
             'top-24',
             'w-[85%] max-w-sm rounded-lg shadow-lg',
             'px-4 py-6 space-y-6',
+            'bg-gray-900',
             isMenuRendered && styles.menuRendered
           )}
         >
